57e09fdcea99babe694ff9b3ed532b9cfde7372a
vknir <kutkarsh.umar@gmail.com>
2024-12-31 19:56:13 +0530

    C:/Program Files/Git/about-me section added

--- portfolio/app/about-me/page.tsx
+++ portfolio/app/about-me/page.tsx
@@ -1,3 +1,13 @@
 export default function Aboutme(){
-    return <div>hello</div>
+    return<div className="overflow-auto scrollbar-hide flex">
+        <div className="w-72 h-full border border-gray-700 border-y-0 border-r border-l-0"> {/* left column*/}
+            <div className="h-full w-32 border border-gray-700 border-y-0 flex"> {/* icon bar */}
+            </div> 
+
+            <div className=""> {/* text section*/}
+                <div className="text-white"><p>professional-info</p></div>
+            </div>
+        </div> 
+        <div className="w-full h-full"></div> {/*right column */}
+    </div>
 }
