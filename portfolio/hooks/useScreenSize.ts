c53523f099d7d990ffec914614302ead99d75d02
vknir <kutkarsh.umar@gmail.com>
2025-01-07 19:20:38 +0530

    finishing touches part 5

--- portfolio/hooks/useScreenSize.ts
+++ portfolio/hooks/useScreenSize.ts
@@ -3,17 +3,21 @@
 import { useEffect, useState } from "react";
 
 export default function useScreenSize(query: string) {
-  const [result, setResult] = useState(()=> window.matchMedia(query).matches);
-  useEffect(() => {
-    const media = window.matchMedia(query);
-    const listener = () => {
-      setResult(media.matches);
-    };
-    window.addEventListener("resize", listener);
+  if (typeof window !== undefined) {
+    const [result, setResult] = useState(
+      () => window.matchMedia(query).matches
+    );
+    useEffect(() => {
+      const media = window.matchMedia(query);
+      const listener = () => {
+        setResult(media.matches);
+      };
+      window.addEventListener("resize", listener);
 
-    return () => {
-      window.removeEventListener("resize", listener);
-    };
-  }, [query]);
-  return result;
+      return () => {
+        window.removeEventListener("resize", listener);
+      };
+    }, [query]);
+    return result;
+  }
 }
